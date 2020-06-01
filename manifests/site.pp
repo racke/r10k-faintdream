1454c1151b97c0640dc1184eec961ef75fc32296
Akash <dubey.akash@gmail.com>
2020-06-01 11:59:58 +0530

    lint correction

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,17 +1,17 @@
 node /^puppet[.]\w+[.]\w+$/ {
- include r10k
- include eyaml
+  include r10k
+  include eyaml
 }
 node /^node[.]\w+[.]\w+$/ {
- include buildtools
- include apache
- include hierasample
- Class["buildtools"] -> Class["apache"] ->Class["hierasample"]
+  include buildtools
+  include apache
+  include hierasample
+  Class["buildtools"] -> Class["apache"] ->Class["hierasample"]
 }
 
 node /^ubuntu[.]\w+[.]\w+$/ {
- include buildtools
- include apache
+  include buildtools
+  include apache
 }
 
 include hierasample::secrets
