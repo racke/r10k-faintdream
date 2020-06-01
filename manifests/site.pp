364db8076c4e59f2e60c0b2eef40f43a30f6fb0d
Akash <dubey.akash@gmail.com>
2020-06-01 11:59:11 +0530

    make node def. match regular expression

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,15 @@
-node 'puppet' {
+node /^puppet[.]\w+[.]\w+$/ {
  include r10k
  include eyaml
 }
-node 'node' {
+node /^node[.]\w+[.]\w+$/ {
  include buildtools
  include apache
  include hierasample
  Class["buildtools"] -> Class["apache"] ->Class["hierasample"]
 }
 
-node 'ubuntu' {
+node /^ubuntu[.]\w+[.]\w+$/ {
  include buildtools
  include apache
 }
